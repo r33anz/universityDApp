21dd4d732d208d9c9846d14f708420416e62a34f
r33anz <anzaldoalvaradorodrigo@gmail.com>
2025-03-22 15:37:44 -0400

    [UPDATE] events to manage request of kardex

--- hardhat/contracts/CredentialStudentsManagement.sol
+++ hardhat/contracts/CredentialStudentsManagement.sol
@@ -4,34 +4,32 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract CredentialStudentManagement is Ownable{
+contract CredentialStudentManagement is Ownable {
     using ECDSA for bytes32;
-
-    struct Student{
-        string codSIS; 
+    
+    struct Student {
+        string codSIS;
         address walletAddress;
         bytes32 passwordHash;
         bytes publicKey;
         string ipfsHash;
         uint256 issuedAt;
     }
-
-    mapping (string => Student) public students; // SISCode -> Student 
-    mapping (address => string) public walletToCodSIS; // Wallet -> SISCode
     
-    event CredentialStudentEmmited(string indexed studentSIS, address indexed studentWallet);
+    mapping (string => Student) public students; // SISCode -> Student
+    
+    event RequestKardex(string indexed codSIS, uint256 timeRequested);
+    event ResponseKardex(string indexed codSIS, address studentAddressWallet, uint256 timeRequested);
     
     constructor() Ownable(msg.sender) {
-
     }
-
+    
     function emmitCredential(
         string calldata studentSIS,
         address studentWallet,
         bytes calldata studentPublicKey
-    ) public onlyOwner{
-
-        require(bytes(students[studentSIS].codSIS).length == 0, 
+    ) public onlyOwner {
+        require(bytes(students[studentSIS].codSIS).length == 0,
             "Student already exists");
 
         students[studentSIS] = Student({
@@ -42,29 +40,38 @@ contract CredentialStudentManagement is Ownable{
             publicKey: studentPublicKey,
             issuedAt: block.timestamp
         });
-
-        walletToCodSIS[studentWallet] = studentSIS;
-
-        emit CredentialStudentEmmited(studentSIS, studentWallet);         
     }
+    
+    function setStudentPassword(string calldata sisCode, bytes32 passwordHash) external {
+        
+        require(msg.sender == students[sisCode].walletAddress, 
+            "Only student or owner can set password");
 
-    function setStudentPassword(string calldata sisCode, bytes32 passwordHash) public onlyOwner{
         students[sisCode].passwordHash = passwordHash;
     }
-
-    function verifySISCodeByWalletAddres(address walletAddress) external view returns(string memory){
-        return walletToCodSIS[walletAddress];
+    
+    function setIPFSHash(string calldata sisCode, string calldata ipfsHash) external onlyOwner {
+        address studentAddressWallet = students[sisCode].walletAddress;
+        students[sisCode].ipfsHash = ipfsHash;
+        
+        emit ResponseKardex(sisCode, studentAddressWallet, block.timestamp);
     }
+    
+    function getAddressAndIPFSHash(string calldata sisCode) external view returns(address, string memory) {
+        return (students[sisCode].walletAddress, students[sisCode].ipfsHash);
+    }
+    
+    function getStudentPassword(string calldata sisCode) external view returns(bytes32) {
+        
+        require(msg.sender == students[sisCode].walletAddress, 
+            "Only student or owner can view password hash");
 
-    function getStudentPassword(string calldata sisCode) external view returns(bytes32){
         return students[sisCode].passwordHash;
     }
-
-    function setIPFSHash(string calldata sisCode, string calldata ipfsHash) public onlyOwner{
-        students[sisCode].ipfsHash = ipfsHash; 
-    }
-
-    function getAddressAndIPFSHash(string calldata sisCode) external view returns(address, string memory){
-        return (students[sisCode].walletAddress, students[sisCode].ipfsHash);
+    
+    function requestKardex(string calldata codSIS) external {
+        require(bytes(students[codSIS].codSIS).length > 0, "Student does not exist");
+        
+        emit RequestKardex(codSIS, block.timestamp);
     }
 }
