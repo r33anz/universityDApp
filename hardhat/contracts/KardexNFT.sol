91acef105308637ec5ef1fc6f4c46b401cbd0c30
r33anz <anzaldoalvaradorodrigo@gmail.com>
2025-08-31 22:23:38 -0400

    [UPDATE] nft version solidity

--- hardhat/contracts/KardexNFT.sol
+++ hardhat/contracts/KardexNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.22;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
