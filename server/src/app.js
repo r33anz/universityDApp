8b7f289d386138d76b9a2f23ccd2aee1f788790f
r33anz <anzaldoalvaradorodrigo@gmail.com>
2026-07-07 13:34:43 -0400

    [UPDATE] config

--- server/src/app.js
+++ server/src/app.js
@@ -1,26 +1,25 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import {createServer} from "http";
+import { createServer } from "http";
 import { Server } from "socket.io";
+import config from "./infrastructure/config/env.js";
 import studentRoutes from "./interface/routes/studentRoutes.js";
-import notificationRoutes from "./interface/routes/notificationRoutes.js"
+import notificationRoutes from "./interface/routes/notificationRoutes.js";
 import ListenBlockchainEvent from "./interface/events/ListenBlockchainEvent.js";
 import pdfRoutes from "./interface/routes/pdfRoutes.js";
-
-dotenv.config();
+import errorHandler from "./interface/middleware/errorHandler.js";
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer,{
-    cors:"*",
-    methods:["GET","POST"]
+const io = new Server(httpServer, {
+    cors: { origin: config.http.corsOrigin, methods: ["GET", "POST"] },
 });
-app.use(cors());    
+app.use(cors({ origin: config.http.corsOrigin }));
 app.use(express.json());
 app.use('/api',studentRoutes);
 app.use('/api',notificationRoutes);
 app.use('/api',pdfRoutes);
+app.use(errorHandler);
 
 io.on("connection", (socket) => {
     console.log("Nuevo cliente conectado:", socket.id);
--- server/src/index.js
+++ server/src/index.js
@@ -1,27 +1,19 @@
-import dotenv from "dotenv";
-import {app,io} from "./app.js";
-import ipfsConnection from "./infraestructure/ipfs/ipfsConnection.js";
-import sequelize from "./infraestructure/db/dbConnection.js";
+import config from "./infrastructure/config/env.js";
+import { app, io } from "./app.js";
+import ipfsConnection from "./infrastructure/ipfs/ipfsConnection.js";
+import sequelize from "./infrastructure/db/dbConnection.js";
 
-dotenv.config();
-
-const PORT = process.env.PORT || 5000;
-
-sequelize.sync({ force: false })  
+sequelize.sync({ force: false })
     .then(() => console.log('Base de datos sincronizada'))
     .catch(err => console.error('Error al sincronizar BD:', err));
 
-
-app.listen(PORT, async () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(config.http.port, async () => {
+    console.log(`Server running on port ${config.http.port}`);
     try {
         await ipfsConnection.initialize();
         console.log("IPFS inicializado");
-
-        io.emit("server_ready", { message: "Servidor listo", time: new Date() });
-    
     } catch (error) {
-        console.error("Error ", error)       
+        console.error("Error ", error);
         process.exit(1);
     }
 });
