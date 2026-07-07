49e54d899b638b439cbe7229401edbb6a510db8e
r33anz <anzaldoalvaradorodrigo@gmail.com>
2026-07-07 12:34:30 -0400

    [UPDATE] configuration front project

--- fronted/tailwind.config.js
+++ fronted/tailwind.config.js
@@ -3,8 +3,30 @@ module.exports = {
       "./src/**/*.{js,jsx}",
       "./public/index.html"
     ],
+    darkMode: 'class',
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          brand: {
+            blue: '#184494',
+            teal: '#107e7d',
+            gold: '#e3b505',
+            red: '#e80414',
+          },
+          surface: {
+            light: '#f8f9fb',
+            dark: '#0f1729',
+          },
+          card: {
+            light: '#ffffff',
+            dark: '#1a2540',
+          },
+          muted: {
+            light: '#f1f3f7',
+            dark: '#1e2d4a',
+          },
+        },
+      },
     },
     plugins: [],
   }
--- fronted/webpack.config.js
+++ fronted/webpack.config.js
@@ -1,60 +1,74 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[path][name].[ext]',
-            },
-          },
-        ],
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(process.env)
-    }),
-  ],
-  devServer: {
-    historyApiFallback: true,
-    static: {
-      directory: path.join(__dirname, 'public'),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction ? '[name].[contenthash:8].js' : 'bundle.js',
+      chunkFilename: isProduction ? '[name].[contenthash:8].js' : '[name].bundle.js',
+      publicPath: '/',
+      clean: true,
     },
-    port: 3001,
-    open: true,
-  }
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'postcss-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/[name].[hash:8][ext]',
+          },
+        },
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify(
+          Object.fromEntries(
+            Object.entries(process.env).filter(([key]) => key.startsWith('REACT_APP_'))
+          )
+        )
+      }),
+      ...(isProduction ? [new MiniCssExtractPlugin({
+        filename: '[name].[contenthash:8].css',
+      })] : []),
+    ],
+    devServer: {
+      historyApiFallback: true,
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      port: 3001,
+      open: true,
+    }
+  };
 };
